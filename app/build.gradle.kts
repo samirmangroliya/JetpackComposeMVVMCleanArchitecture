f69fa1f5935de3302e52bb06cfa56a4916ef6fa2
Samir Mangroliya <sbmangroliya2012@gmail.com>
2025-12-02 11:52:16 +0530

    added new library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,17 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.ksp)
+}
+
+val secretPropertiesFile: File = rootProject.file("secrets.properties")
+val sitProps = Properties()
+secretPropertiesFile.inputStream().use { input ->
+    sitProps.load(input)
 }
 
 android {
@@ -18,26 +28,54 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        buildConfigField("String", "prefPassword", sitProps.getProperty("prefPassword"))
+        buildConfigField("String", "prefName", sitProps.getProperty("prefName"))
+        buildConfigField("String", "API_KEY", sitProps.getProperty("API_KEY"))
+        buildConfigField("String", "baseUrl", sitProps.getProperty("baseUrl"))
+
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+        debug {
+            isMinifyEnabled = false
+            isDebuggable = true
+            isShrinkResources = false
+        }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
+        viewBinding = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get() as String?
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
@@ -50,6 +88,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    //testing library
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +96,60 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.inline)
+    //For runBlockingTest, CoroutineDispatcher etc.
+    testImplementation(libs.kotlinx.coroutines.test)
+    //For InstantTaskExecutorRule
+    testImplementation(libs.androidx.core.testing)
+    // Core library
+    androidTestImplementation(libs.androidx.core)
+    implementation(libs.lottie.compose)
+    implementation(libs.shimmer.compose)
+
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.rules)
+    androidTestUtil(libs.androidx.test.orchestrator)
+
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.room.ktx)
+
+    // Assertions
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.truth)
+    androidTestImplementation(libs.google.test.truth)
+
+    // Espresso dependencies
+    androidTestImplementation(libs.test.espresso.core)
+    androidTestImplementation(libs.test.espresso.contrib)
+    androidTestImplementation(libs.test.espresso.intents)
+    androidTestImplementation(libs.test.espresso.accessibility)
+    androidTestImplementation(libs.test.idling.concurrent)
+
+    implementation(libs.mockito.kotlin)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.test)
+    //Hilt
+    implementation(libs.dagger.hilt.android)
+    ksp(libs.dagger.compiler)
+    ksp(libs.dagger.hilt.android.compiler)
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.okhttp)
+    implementation(libs.converter.scalars)
+    implementation(libs.encrypted.preferences)
+    //Glide
+    implementation(libs.landscapist.glide)
+
+    implementation(libs.androidx.navigation.compose)
+    //room
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    testImplementation(libs.androidx.room.testing)
+    ksp(libs.androidx.room.compiler)
+    implementation(kotlin("test"))
+
 }
